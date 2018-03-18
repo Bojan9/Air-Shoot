4923f56b0df93ae1c5cc1584e8da59096719acfd
Bojan9 <9spasovski@gmail.com>
2018-03-18 21:54:21 +0100

    probuvav nekolku nacini no ne mi uspeva da gi snema kockite koga ke se doprat, gi smeniv i slikite na bullets

--- js/main.js
+++ js/main.js
@@ -1,57 +1,55 @@
 $(function () {
 
 
-//    $(".wrap").on("mouseenter", function () {
-//        console.log("vleze");
-//    }).on("mouseleave", function () {
-//        console.log("izleze");
-//    })
+    //    $(".wrap").on("mouseenter", function () {
+    //        console.log("vleze");
+    //    }).on("mouseleave", function () {
+    //        console.log("izleze");
+    //    })
 
     $(".wrap").mousemove(function (event) {
         var relX = event.pageX - $(this).offset().left;
         var relY = event.pageY - $(this).offset().top;
         var relBoxCoords = "(" + relX + "," + relY + ")";
-       // console.log(relBoxCoords);
+        // console.log(relBoxCoords);
 
-//        $(".airplane").css({
-//            "top": relY + "px",
-//            "left": relX + "px",
-//        })
+        //        $(".airplane").css({
+        //            "top": relY + "px",
+        //            "left": relX + "px",
+        //        })
 
-        document.getElementsByClassName('airplane')[0].style = "transform: translate("+relX+"px,"+relY+"px )";
+        document.getElementsByClassName('airplane')[0].style = "transform: translate(" + relX + "px," + relY + "px )";
     });
 
-    $(".wrap").on("click", function(event){
+    $(".wrap").on("click", function (event) {
         var relX = event.pageX - $(this).offset().left;
         var relY = event.pageY - $(this).offset().top;
-//        var element = "<div class='bullet' style='transform: translate("+(relX+20)+"px,"+relY+"px )'></div>";
-        var element = "<div class='bullet' style='top:"+relY+"px;left:"+(relX+20)+"px'></div>";
+        //        var element = "<div class='bullet' style='transform: translate("+(relX+20)+"px,"+relY+"px )'></div>";
+        var element = "<div class='bullet' style='top:" + relY + "px;left:" + (relX + 20) + "px'></div>";
         $(this).append(element);
     })
 
-    setInterval(function(){
-        var bullets =  $(".bullet");
+    setInterval(function () {
+        var bullets = $(".bullet");
         var blocks = $(".block");
-        if(bullets.length){
-           for(var i = 0; i<bullets.length; i++){
-               var newTop = parseInt($(bullets[i]).css("top"))-10;
-               //console.log(newTop);
-               if(newTop<0){
-                   $(bullets[i]).remove();
-               }else{
-                   for(var j = 0; j<blocks.length; j++){
-                       if(bullets.css("top") < (blocks.css("top") + blocks.css("height"))){
-                           if(bullets.css("left") > blocks.css("left") && bullets.css("left") < (blocks.css("left") + blocks.css("width"))){
-                               $(bullets[i]).remove();
-                               $(blocks[i]).remove();
-                           }else{}
-                       }else{}
-                   }
-                   $(bullets[i]).css("top", newTop)
-               }
-           }
-
+        if (bullets.length) {
+            for (var i = 0; i < bullets.length; i++) {
+                var newTop = parseInt($(bullets[i]).css("top")) - 10;
+                //console.log(newTop);
+                if (newTop < 0) {
+                    $(bullets[i]).remove();
+                } else {
+                    for (var j = 0; j < blocks.length; j++) {
+                        if (bullets.css("top") < (blocks.css("top") + blocks.css("height"))) {
+                            if (bullets.css("left") > blocks.css("left") && bullets.css("left") < (blocks.css("left") + blocks.css("width"))) {
+                                $(bullets[i]).remove();
+                                $(blocks[i]).remove();
+                            } else {}
+                        } else {}
+                    }
+                    $(bullets[i]).css("top", newTop)
+                }
+            }
         }
-
-    },100)
+    }, 100)
 })
